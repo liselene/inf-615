483faf354d4029a551220f998ea95261f8e19842
mscarpim <mscarpim@motorola.com>
2018-08-06 17:51:52 -0300

    Trata feature discreta, remove linhas com NA e aplica normalizacao

--- trab1/trab1.R
+++ trab1/trab1.R
@@ -1,29 +1,56 @@
 rm(list=ls())
-setwd("~/Projects/ComplexData/inf-615/trab1")
 
 valSet<-read.csv("housePricing_valSet.csv")
 trainSet<-read.csv("housePricing_trainSet.csv")
 
 ### 1 - Inspecione os dados. Quantos exemplos você tem?
-paste("Examples de dados de treinamento:", nrow(trainSet))
+paste("Exemplos de dados de treinamento:", nrow(trainSet))
 paste("Exemplos de dados de validacao:",nrow(valSet))
 
 # Como você irá lidar com as features discretas? 
-paste("As features seriam mapeadas em uma escala de valores inteiros")
+paste("As features serão mapeadas em disversas colunas de binários")
 niveis<-unique(trainSet$ocean_proximity)
+trainSet$hocean = as.numeric(trainSet$ocean_proximity == niveis[1])
+trainSet$near_bay = as.numeric(trainSet$ocean_proximity == niveis[2])
+trainSet$inland = as.numeric(trainSet$ocean_proximity == niveis[3])
+trainSet$near_ocean = as.numeric(trainSet$ocean_proximity == niveis[4])
+trainSet$island = as.numeric(trainSet$ocean_proximity == niveis[5])
+trainSet$ocean_proximity = NULL
+
+# TODO: precisa arrumar o factor
+valSet$hocean = as.numeric(valSet$ocean_proximity == niveis[1])
+valSet$near_bay = as.numeric(valSet$ocean_proximity == niveis[2])
+valSet$inland = as.numeric(valSet$ocean_proximity == niveis[3])
+valSet$near_ocean = as.numeric(valSet$ocean_proximity == niveis[4])
+valSet$island = as.numeric(valSet$ocean_proximity == niveis[5])
+valSet$ocean_proximity = NULL
 
 # Há exemplos com features sem anotações? 
 paste("Sim")
 
 # Como você lidaria com isso?
 paste("Remove-se as amostras sem anotação, caso a variavel seja utilizada no treinamento, caso contrário manteria.")  
-valSet_c<-valSet[valSet!=NA]
-trainSet_c<-trainSet[!is.na(trainSet)]
+trainSet <- trainSet[rowSums(is.na(trainSet)) == 0,]
 
 ## 2- Normalize os dados de modo que eles fiquem todos no mesmo intervalo.
-mean(valSet$longitude)
-sapply(valSet_c,mean)
-sapply(valSet_c,std)
+##    Não normalizaremos latitude e longitude, bem como o preço final
+trainSet_save <- trainSet
+
+meanTrainFeatures = colMeans(trainSet[,3:8]) #mean of each feature
+stdTrainFeatures = apply(trainSet[,3:8], 2, sd) #std of each feature
+
+meanTrainFeatures
+stdTrainFeatures
+
+# some weird error is happening here
+trainSet[,3:8] <- sweep(trainSet[,3:8], 2, meanTrainFeatures, "-")
+trainSet[,3:8] <- trainSet[,3:8] / stdTrainFeatures
+
+valSet[,3:8] <- sweep(valSet[,3:8], 2, meanTrainFeatures, "-")
+valSet[,3:8] <- valSet[,3:8] / stdTrainFeatures
+
+# Regressão Linear
+# TODO: Aplicar regressão
 
 # nrow(samples)
 # ncol(samples)
