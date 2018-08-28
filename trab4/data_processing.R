361ccf4f53193add0a8be28c11c3b5a173dae70a
mscarpim <mscarpim@motorola.com>
2018-08-28 17:09:52 -0300

    Starting model - not working yet

--- trab4/data_processing.R
+++ trab4/data_processing.R
@@ -2,78 +2,94 @@
 # Trabalho 4 - INF-615 - Data treatment
 # Nome(s): Liselene Borges e Marcos Scarpim
 ########################################
-library(dplyr)
-
 set.seed(42)
 
 data = read.csv("mnist_trainVal.csv", header=FALSE)
 
 # convert output to factor
-data[,1] <- as.factor(data[,1])
+#data[,1] <- as.factor(data[,1])
 summary(data[,1])
 
 # apply PCA or normalization here
 #remove "only zero columns"
 data_filtered <- data[,c(TRUE, colSums(data[,2:ncol(data)]) != 0)]
-
+# apply PCA
 data.pca1 <- prcomp(data_filtered[,2:ncol(data_filtered)], scale.=TRUE)
+cumsum(data.pca1$sdev^2 / sum(data.pca1$sdev^2)) # 95% -> 331
+
+# get PCA with 95% of variance
+#data_95_var <- data.frame(V1 = data[,1], data.pca1$x[,1:331])
+# get PCA with 60% of variance for testing!
+data_60_var <- data.frame(V1 = data[,1], data.pca1$x[,1:64])
 
 
-split_data_train <- list()
-split_data_val <- list()
-# split data
-data1 <- data[data$V1 == 1, ]
-idx <- sample(1:nrow(data1), 0.8*nrow(data1))
-split_data_train[[1]] <- data1[idx,]
-split_data_val[[1]] <- data1[-idx,]
+split_data <- function(data) {
+  split_data_train <- list()
+  split_data_val <- list()
+  # split data
+  data1 <- data[data$V1 == 1, ]
+  idx <- sample(1:nrow(data1), 0.8*nrow(data1))
+  split_data_train[[1]] <- data1[idx,]
+  split_data_val[[1]] <- data1[-idx,]
+  
+  data2 <- data[data$V1 == 2, ]
+  idx <- sample(1:nrow(data2), 0.8*nrow(data2))
+  split_data_train[[2]] <- data2[idx,]
+  split_data_val[[2]] <- data2[-idx,]
+  
+  data3 <- data[data$V1 == 3, ]
+  idx <- sample(1:nrow(data3), 0.8*nrow(data3))
+  split_data_train[[3]] <- data3[idx,]
+  split_data_val[[3]] <- data3[-idx,]
+  
+  data4 <- data[data$V1 == 4, ]
+  idx <- sample(1:nrow(data4), 0.8*nrow(data4))
+  split_data_train[[4]] <- data4[idx,]
+  split_data_val[[4]] <- data4[-idx,]
+  
+  data5 <- data[data$V1 == 5, ]
+  idx <- sample(1:nrow(data5), 0.8*nrow(data5))
+  split_data_train[[5]] <- data5[idx,]
+  split_data_val[[5]] <- data5[-idx,]
+  
+  data6 <- data[data$V1 == 6, ]
+  idx <- sample(1:nrow(data6), 0.8*nrow(data6))
+  split_data_train[[6]] <- data6[idx,]
+  split_data_val[[6]] <- data6[-idx,]
+  
+  data7 <- data[data$V1 == 7, ]
+  idx <- sample(1:nrow(data7), 0.8*nrow(data7))
+  split_data_train[[7]] <- data7[idx,]
+  split_data_val[[7]] <- data7[-idx,]
+  
+  data8 <- data[data$V1 == 8, ]
+  idx <- sample(1:nrow(data8), 0.8*nrow(data8))
+  split_data_train[[8]] <- data8[idx,]
+  split_data_val[[8]] <- data8[-idx,]
+  
+  data9 <- data[data$V1 == 9, ]
+  idx <- sample(1:nrow(data9), 0.8*nrow(data9))
+  split_data_train[[9]] <- data9[idx,]
+  split_data_val[[9]] <- data9[-idx,]
+  
+  data0 <- data[data$V1 == 0, ]
+  idx <- sample(1:nrow(data0), 0.8*nrow(data0))
+  split_data_train[[10]] <- data0[idx,]
+  split_data_val[[10]] <- data0[-idx,]
+  
+  # join all validation data together
+  valData = do.call("rbind", split_data_val)
+  return(list(valData, split_data_train, split_data_val))
+}
 
-data2 <- data[data$V1 == 2, ]
-idx <- sample(1:nrow(data2), 0.8*nrow(data2))
-split_data_train[[2]] <- data2[idx,]
-split_data_val[[2]] <- data2[-idx,]
+# split data into train and val
+splitted_data <- split_data(data_60_var)
+valData <- splitted_data[[1]]
+split_data_train <- splitted_data[[2]]
+split_data_val <- splitted_data[[3]]
 
-data3 <- data[data$V1 == 3, ]
-idx <- sample(1:nrow(data3), 0.8*nrow(data3))
-split_data_train[[3]] <- data3[idx,]
-split_data_val[[3]] <- data3[-idx,]
 
-data4 <- data[data$V1 == 4, ]
-idx <- sample(1:nrow(data4), 0.8*nrow(data4))
-split_data_train[[4]] <- data4[idx,]
-split_data_val[[4]] <- data4[-idx,]
 
-data5 <- data[data$V1 == 5, ]
-idx <- sample(1:nrow(data5), 0.8*nrow(data5))
-split_data_train[[5]] <- data5[idx,]
-split_data_val[[5]] <- data5[-idx,]
-
-data6 <- data[data$V1 == 6, ]
-idx <- sample(1:nrow(data6), 0.8*nrow(data6))
-split_data_train[[6]] <- data6[idx,]
-split_data_val[[6]] <- data6[-idx,]
-
-data7 <- data[data$V1 == 7, ]
-idx <- sample(1:nrow(data7), 0.8*nrow(data7))
-split_data_train[[7]] <- data7[idx,]
-split_data_val[[7]] <- data7[-idx,]
-
-data8 <- data[data$V1 == 8, ]
-idx <- sample(1:nrow(data8), 0.8*nrow(data8))
-split_data_train[[8]] <- data8[idx,]
-split_data_val[[8]] <- data8[-idx,]
-
-data9 <- data[data$V1 == 9, ]
-idx <- sample(1:nrow(data9), 0.8*nrow(data9))
-split_data_train[[9]] <- data9[idx,]
-split_data_val[[9]] <- data9[-idx,]
-
-data0 <- data[data$V1 == 0, ]
-idx <- sample(1:nrow(data0), 0.8*nrow(data0))
-split_data_train[[10]] <- data0[idx,]
-split_data_val[[10]] <- data0[-idx,]
-
-# join all validation data together
-valData = do.call("rbind", split_data_val)
 
 
 
--- trab4/trabalho_4.R
+++ trab4/trabalho_4.R
@@ -3,6 +3,146 @@
 # Nome(s): Liselene Borges e Marcos Scarpim
 ########################################
 
+library(neuralnet)
+
+setwd("~/Documents/Curso - Complex Data/INF-0615/Tarefa1/inf-615/trab4")
+
 # create and process data
 source("data_processing.R")
 set.seed(42)
+
+# define the formula
+feats <- names(valData)
+f <- paste(feats[2:length(feats)],collapse=' + ')
+f <- paste('V1 ~',f)
+f <- as.formula(f)
+
+############### 1 vs Rest
+positiveTrainData <- split_data_train[[1]]
+positiveTrainData$V1 <- 1
+
+negativeTrainData <- do.call("rbind", split_data_train[2:10])
+negativeTrainData$V1 <- 0
+
+trainData <- rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+idx <- sample(1:nrow(trainData), 0.8*nrow(trainData))
+trainDataTest <- trainData[-idx,]
+nrow(trainDataTest)
+
+NN1 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 2 vs Rest
+positiveTrainData <- split_data_train[[2]]
+positiveTrainData$V1 <- 1
+
+negativeTrainData <- do.call("rbind", append(split_data_train[1], split_data_train[3:10]))
+negativeTrainData2$V1 <- 0
+
+trainData <- rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+idx <- sample(1:nrow(trainData), 0.8*nrow(trainData))
+trainDataTest <- trainData[-idx,]
+nrow(trainDataTest)
+
+NN2 = neuralnet(formula=f, data=trainDataTest, hidden=c(3,3), linear.output=FALSE)
+
+############### 3 vs Rest
+positiveTrainData <- split_data_train[[3]]
+positiveTrainData$V1 <- 1
+
+negativeTrainData <- do.call("rbind", append(split_data_train[1:2], split_data_train[4:10]))
+negativeTrainData$V1 <- 0
+
+trainData <- rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN3 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 4 vs Rest
+positiveTrainData <- split_data_train[[4]]
+positiveTrainData$V1<- 1
+
+negativeTrainData <- do.call("rbind", append(split_data_train[1:3], split_data_train[5:10]))
+negativeTrainData$V1 <- 0
+
+trainData <- rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN4 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 5 vs Rest
+positiveTrainData = split_data_train[[5]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", append(split_data_train[1:4], split_data_train[6:10]))
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN5 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 6 vs Rest
+positiveTrainData = split_data_train[[6]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", append(split_data_train[1:5], split_data_train[7:10]))
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN6 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 7 vs Rest
+positiveTrainData = split_data_train[[7]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", append(split_data_train[1:6], split_data_train[8:10]))
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN7 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 8 vs Rest
+positiveTrainData = split_data_train[[8]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", append(split_data_train[1:7], split_data_train[9:10]))
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN8 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 9 vs Rest
+positiveTrainData = split_data_train[[9]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", append(split_data_train[1:8], split_data_train[10]))
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN9 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+############### 0 vs Rest
+positiveTrainData = split_data_train[[10]]
+positiveTrainData$V1 = 1
+
+negativeTrainData = do.call("rbind", split_data_train[1:9])
+negativeTrainData$V1 = 0
+
+trainData = rbind(positiveTrainData, negativeTrainData)
+nrow(trainData)
+
+NN0 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+
+
