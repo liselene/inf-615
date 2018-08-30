90787b21a07e612ff2727949bc2b0703b32e271c
mscarpim <mscarpim@motorola.com>
2018-08-30 17:53:21 -0300

    remove unused code

--- trab4/trabalho_4.R
+++ trab4/trabalho_4.R
@@ -9,9 +9,9 @@ library(neuralnet)
 #install.packages("e1071")
 library(e1071)
 
-#setwd("~/Documents/Curso - Complex Data/INF-0615/Tarefa1/inf-615/trab4")
+setwd("~/Documents/Curso - Complex Data/INF-0615/Tarefa1/inf-615/trab4")
 #setwd("~/Documents/UNICAMP/Curso - Mineracao/INF-0615/inf-615/trab4")
-setwd("~/Projects/ComplexData/inf-615/trab4")
+#setwd("~/Projects/ComplexData/inf-615/trab4")
 
 # create and process data
 source("data_processing.R")
@@ -47,77 +47,16 @@ for (i in 1:10) {
   trainData <- getBalancedData(split_data_train, i)
   print(paste0("Aplying model ", i, ", data size = " , nrow(trainData)))
   
-  NN[[i]] <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
+  NN[[i]] <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE, stepmax = 1e6)
 }
 
-############### 1 vs Rest
-.f <- function() {
-trainData <- getBalancedData(split_data_train, 1)
-nrow(trainData)
-
-NN1 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 2 vs Rest
-trainData <- getBalancedData(split_data_train, 2)
-nrow(trainData)
-
-NN2 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 3 vs Rest
-trainData <- getBalancedData(split_data_train, 3)
-nrow(trainData)
-
-NN3 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 4 vs Rest
-trainData <- getBalancedData(split_data_train, 4)
-nrow(trainData)
-
-NN4 <- neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 5 vs Rest
-trainData <- getBalancedData(split_data_train, 5)
-nrow(trainData)
-
-NN5 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 6 vs Rest
-trainData <- getBalancedData(split_data_train, 6)
-nrow(trainData)
-
-NN6 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 7 vs Rest
-trainData <- getBalancedData(split_data_train, 7)
-nrow(trainData)
-
-NN7 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 8 vs Rest
-trainData <- getBalancedData(split_data_train, 8)
-nrow(trainData)
-
-NN8 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 9 vs Rest
-trainData <- getBalancedData(split_data_train, 9)
-nrow(trainData)
-
-NN9 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-############### 0 vs Rest
-trainData <- getBalancedData(split_data_train, 10)
-nrow(trainData)
-
-NN0 = neuralnet(formula=f, data=trainData, hidden=c(3,3), linear.output=FALSE)
-
-}
-
+.f2 <- function() {
 ## svm
 trainData <- getBalancedData(split_data_train, 1)
 svm.model <- svm(f, data = trainData, cost = 100, gamma = 1)
 svm.pred  <- predict(svm.model, valData)
 CM = as.matrix(table(Actual = valData$V1, Predicted = svm.pred))
+}
 #TPR = CM[2,2] / (CM[2,2] + CM[2,1])
 #TNR = CM[1,1] / (CM[1,1] + CM[1,2])
 #ACCNorm = mean(c(TPR, TNR))
